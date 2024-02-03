53a7f441ab26ef956480717cb2d9c516c10afd15
P N Unnathi <pnunnathi@gmail.com>
2024-02-03 10:07:54 +0530

    Delete admin.py

--- admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from base.models.customer import Customer
-from base.models.useractivity import UserActivity
-# Register your models here.
-admin.site.register(Customer)
-admin.site.register(UserActivity)
